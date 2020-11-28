974c74d7160dc9c8cfaad14c8279fed6557ccfd8
Camden Rogers <camdenrogers@att.net>
2020-11-28 10:32:27 -0500

    add age consent on migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_165147) do
+ActiveRecord::Schema.define(version: 2020_11_28_153104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -155,6 +155,7 @@ ActiveRecord::Schema.define(version: 2020_10_22_165147) do
     t.string "affiliation", default: ""
     t.string "resume", default: ""
     t.string "remote_location", default: ""
+    t.boolean "age_consent", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
