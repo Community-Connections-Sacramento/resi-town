106f3e3b1b1469f60c5e4066b3562dedb001ae98
allenorton <norton.allene@gmail.com>
2020-12-20 17:17:11 -0600

    added and renamed images/svgs, edited team, edited categories in settings

--- config/initializers/team.rb
+++ config/initializers/team.rb
@@ -2,16 +2,24 @@
 # frozen_string_literal: true
 
 TEAM = {
-    'ResiTown Sacramento Team': [
-        ['Ryan Skaggs', 'Founder, Executive Director', 'Public health advocate, musician, and social entrepreneur dedicated to facilitating positive change.'],
-        ['Jessica Lin', 'Director of Community Engagement', 'Jessica is Public Health Strategist at California Department of Public Health.'],
-        ['Miriam Sanchez', 'Director of Operations', 'Miriam is a MPH student at CSUN with a background in nutrition, public health, and holistic health.'],
-        ['Jin Tran', 'Director of Policy and Programs', 'No description provided.'],
+    'ResiTown Sacramento Board Of Directors': [
+        ['Ryan Skaggs', 'Founder, Executive Director', 'Public health advocate and social entrepreneur dedicated to advancing positive change.'],
+        ['Jessica Lin', 'Director of Community Engagement', 'Health equity champion with an extensive background in global health.'],
+        ['Miriam Sanchez', 'Director of Operations', 'MPH student at CSU Northridge with a background in nutrition and holistic health.'],
+        ['Jin Tran', 'Director of Policy and Planning', 'A social worker passionate about civic engagement and social impact- two areas she has scaled operations for at Facebook..'],
         ['Ellen Chan', 'Director of Communications', 'No description provided.'],
-        ['Allene Norton', 'Software Developer', 'Musician and full-stack software developer.']
+    ],
+    'Student Ambassador Team': [
+        ['Alyssa Rodriguez', 'Public Health student at Sacramento State University.', ''],
+        ['Kira Crawley', 'Public Health student at Sacramento State University.', ''],
+        ['Sana Sheikh', 'Occupational Health student at Sacramento State University.', ''],
+        ['Vy Cao', 'Occupational Health student at Sacramento State University.', ''],
+    ],
+    'City Help + ResiTown Sacramento Development Team': [
+        ['Camden Rogers', 'Platform Manager and Lead Developer', 'Lead Web Developer for City Help and founder of Athens Help With Covid'],
+        ['Allene Norton', 'Software Developer', 'Web Developer for ResiTown Sacramento.'],
     ],
     'City Help Team': [
-        ['Camden Rogers', 'Platform Manager and Lead Developer', 'Lead Web Developer for City Help and founder of Athens Help With Covid'],
         ['Tiffany Chan', 'Founder', 'Founder of New Haven Help With Covid'],
         ['Devin Osborne', 'Community Relations Lead', 'Community relations for New Haven Help With Covid'],
     ],
