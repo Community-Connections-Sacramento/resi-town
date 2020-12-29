3052f26ffa9b2139af297553bfa53c82d65f98cb
allenorton <norton.allene@gmail.com>
2020-12-29 16:26:23 -0500

    fixed gibbon apikey variables

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,1 +1,4 @@
-Gibbon::Request.api_key = ENV['MAILCHIMP_API_KEY']
+# Gibbon::Request.api_key = ENV['MAILCHIMP_API_KEY'] 
+Gibbon::Request.api_key = Rails.application.credentials.gibbon[:MAILCHIMP_API_KEY]
+# Gibbon::Request.logger = Logger.new
+
