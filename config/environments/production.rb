9c0921e5c9237495c9baadb1f2d934d86f351193
allenorton <norton.allene@gmail.com>
2020-12-30 15:00:21 -0500

    edited domain name and set up prod mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "covid_volunteers_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'resi-town-tm6vk.ondigitalocean.app/', protocol: 'https' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -74,15 +77,15 @@ Rails.application.configure do
   # so the no-reply@newhavenhelpwithcovid.com account won't actually be used.
   # The commented out lines below might be on the right track to fix this, but
   # it's completely untested.
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #     address: 'smtp.gmail.com',
-  #     port: 587,
-  #     user_name: Rails.application.credentials.smtp[:SMTP_USERNAME],
-  #     password: Rails.application.credentials.smtp[:SMTP_PASSWORD],
-  #     authentication: :plain,
-  #     enable_starttls_auto: true
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      user_name: ENV['SMTP_USERNAME'],
+      password: ENV['SMTP_PASSWORD'],
+      authentication: :plain,
+      enable_starttls_auto: true
+  }
 
   #config.action_mailer.delivery_method = :smtp
   #config.action_mailer.smtp_settings = {
