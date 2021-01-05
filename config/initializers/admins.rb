04f3fc87c74aaf56a0e34484a05c0c23ec2836be
allenorton <norton.allene@gmail.com>
2021-01-05 13:22:38 -0600

    updated admins

--- config/initializers/admins.rb
+++ config/initializers/admins.rb
@@ -1,3 +1,3 @@
 # ADMINS = ENV['ADMINS']&.split(',') || []
 
-ADMINS = ['norton.allene@gmail.com', 'contact@allenenorton.com', 'jessica.lin@sacconect.com', 'Sana.Sheikh@sacconect.com', 'Vy.Cao@sacconect.com', 'Alyssa.Rodriguez@sacconect.com', 'Kira.Crawley@sacconect.com', 'communityconnections@sacconect.com', '4jintran@gmail.com', 'ellen852@gmail.com', 'jessicalin220@gmail.com', 'mirmsky2@gmail.com']
+ADMINS = ['norton.allene@gmail.com', 'contact@allenenorton.com', 'communityconnections@sacconect.com', '4jintran@gmail.com', 'ellen852@gmail.com', 'jessicalin220@gmail.com', 'mirmsky2@gmail.com']
