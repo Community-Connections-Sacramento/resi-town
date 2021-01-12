45b4b0449ddb88694e6ffc16dffe9e0f32e7dfe0
allenorton <norton.allene@gmail.com>
2021-01-12 14:58:03 -0600

    fixed domain naime for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   #  enable_starttls_auto: true
   #}
 
-  config.action_mailer.default_url_options = { :host => 'newhavenhelpwithcovid.com', protocol: 'http' }
+  config.action_mailer.default_url_options = { :host => 'resitown.com', protocol: 'http' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
